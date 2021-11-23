ea09693ca143ec9169400003ca2895e51c775d86
Daniel Habib Vieira da Silva <danielhvs70@gmail.com>
2021-11-23 01:20:48 -0300

    added main function

--- src/label_maker/core.clj
+++ src/label_maker/core.clj
@@ -1,6 +1,8 @@
 (ns label-maker.core
-  (:require [quil.core :as q]
-            [quil.middleware :as m]))
+  (:require
+    [quil.core :as q]
+    [quil.middleware :as m])
+  (:gen-class))
 
 ;; A4 paper size
 (defn mm-to-px
@@ -14,12 +16,11 @@
 (defn calculate-pos [w h qtd-w qtd-h size-w size-h]
   (let [
         h-size (max size-h (quot h qtd-h))
-        w-size (max size-w (quot w qtd-w))
-        ]
+        w-size (max size-w (quot w qtd-w))]
     (let [ys (filter #(= 0 (rem % h-size)) (range h))
           xs (filter #(= 0 (rem % w-size)) (range w))]
       (for [x xs y ys]
-        [x y])))) 
+        [x y]))))
 
 (defn setup []
   ; Set frame rate frames per second.
@@ -45,7 +46,7 @@
     identity))
 
 (defn the-key-handler [state k]
-  (assoc state 
+  (assoc state
     :qtd-w (max 1 ((key-to-offset-w k) (:qtd-w state)))
     :qtd-h (max 1 ((key-to-offset-h k) (:qtd-h state)))
     :done (= ENTER (:key-code k))))
@@ -56,7 +57,7 @@
         qtd-w (:qtd-w state)
         qtd-h (:qtd-h state)]
     (if (q/loaded? img)
-      {:image img 
+      {:image img
        :all-pos (calculate-pos W H qtd-w qtd-h (.width img) (.height img))
        :qtd-w qtd-w
        :qtd-h qtd-h
@@ -72,7 +73,7 @@
       (do
         (q/background 255)
         (doall
-         (map #(q/image img (first %) (second %)) all-pos))))))
+          (map #(q/image img (first %) (second %)) all-pos))))))
 
 (defn draw [state]
   (when (:all-pos state)
@@ -80,20 +81,22 @@
       (do
         (q/do-record (q/create-graphics W H :pdf "out.pdf")
                      (draw-labels state))
-        (q/exit)) 
+        (q/exit))
       (draw-labels state))))
 
-(q/defsketch label-maker
-  :title "Label Maker"
-  :size [W H]
-  ; setup function called only once, during sketch initialization.
-  :setup setup
-  ; update is called on each iteration before draw.
-  :update update
-  :draw draw
-  :features [:keep-on-top]
-  :key-pressed the-key-handler
-  ; This sketch uses functional-mode middleware.
-  ; Check quil wiki for more info about middlewares and particularly
-  ; fun-mode.
-  :middleware [m/fun-mode m/pause-on-error])
+(defn -main [& _]
+  (q/defsketch label-maker
+    :title "Label Maker"
+    :size [W H]
+    ; setup function called only once, during sketch initialization.
+    :setup setup
+    ; update is called on each iteration before draw.
+    :update update
+    :draw draw
+    :features [:keep-on-top]
+    :key-pressed the-key-handler
+    ; This sketch uses functional-mode middleware.
+    ; Check quil wiki for more info about middlewares and particularly
+    ; fun-mode.
+    :middleware [m/fun-mode m/pause-on-error]))
+

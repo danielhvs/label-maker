a01f22a7a217bcef8759124484964114279b9350
Daniel Habib Vieira da Silva <danielhvs70@gmail.com>
2025-03-10 22:50:08 -0300

    upgrade quil

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :main ^:skip-aot label-maker.core
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [quil "3.0.0"]])
+                 [quil "4.3.1563"]])

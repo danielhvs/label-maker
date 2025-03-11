82ab7711a1f21b6f3609cf321d1d6004f289d665
Daniel Habib Vieira da Silva <danielhvs70@gmail.com>
2025-03-10 22:39:13 -0300

    possible to pass file in

--- src/label_maker/core.clj
+++ src/label_maker/core.clj
@@ -1,7 +1,7 @@
 (ns label-maker.core
   (:require
-    [quil.core :as q]
-    [quil.middleware :as m])
+   [quil.core       :as q]
+   [quil.middleware :as m])
   (:gen-class))
 
 ;; A4 paper size
@@ -14,22 +14,22 @@
 (def ENTER 10)
 
 (defn calculate-pos [w h qtd-w qtd-h size-w size-h]
-  (let [
-        h-size (max size-h (quot h qtd-h))
+  (let [h-size (max size-h (quot h qtd-h))
         w-size (max size-w (quot w qtd-w))]
     (let [ys (filter #(= 0 (rem % h-size)) (range h))
           xs (filter #(= 0 (rem % w-size)) (range w))]
       (for [x xs y ys]
         [x y]))))
 
-(defn setup []
+(defn setup [picture]
+  (println "picture:" picture)
   ; Set frame rate frames per second.
   (q/frame-rate 10)
   ; Set color mode to HSB (HSV) instead of default RGB.
   (q/color-mode :hsb)
   ; setup function returns initial state. It contains
   ; circle color and position.
-  {:image (q/load-image "resources/test.png")
+  {:image (q/load-image (or picture "resources/test.png"))
    :qtd-w 1
    :qtd-h 1})
 
@@ -47,9 +47,9 @@
 
 (defn the-key-handler [state k]
   (assoc state
-    :qtd-w (max 1 ((key-to-offset-w k) (:qtd-w state)))
-    :qtd-h (max 1 ((key-to-offset-h k) (:qtd-h state)))
-    :done (= ENTER (:key-code k))))
+         :qtd-w (max 1 ((key-to-offset-w k) (:qtd-w state)))
+         :qtd-h (max 1 ((key-to-offset-h k) (:qtd-h state)))
+         :done (= ENTER (:key-code k))))
 
 (defn update [state]
   (let [img (:image state)
@@ -73,7 +73,7 @@
       (do
         (q/background 255)
         (doall
-          (map #(q/image img (first %) (second %)) all-pos))))))
+         (map #(q/image img (first %) (second %)) all-pos))))))
 
 (defn draw [state]
   (when (:all-pos state)
@@ -84,12 +84,14 @@
         (q/exit))
       (draw-labels state))))
 
-(defn -main [& _]
+(defn -main
+  "Args: file-path (optional)"
+  [& [picture-path]]
   (q/defsketch label-maker
     :title "Label Maker"
     :size [W H]
     ; setup function called only once, during sketch initialization.
-    :setup setup
+    :setup (partial setup picture-path)
     ; update is called on each iteration before draw.
     :update update
     :draw draw
@@ -100,3 +102,6 @@
     ; fun-mode.
     :middleware [m/fun-mode m/pause-on-error]))
 
+(comment
+  (-main "/home/danielhabib/Downloads/some-file.png")
+  (-main))
